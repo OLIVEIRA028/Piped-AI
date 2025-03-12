6c870f84fcceeec6e22233eed8d797de83564651
João Paulo <155689082+OLIVEIRA028@users.noreply.github.com>
2025-03-12 19:09:45 +0000

    Piped-AI

---
+++ backend_java/src/main/java/com/Piped_AI/backend/PipedaiApplication.java
@@ -0,0 +1,13 @@
+package com.Piped_AI.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipedaiApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipedaiApplication.class, args);
+	}
+
+}
